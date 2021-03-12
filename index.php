3db5b52464d544a340b65aa2b42d280c2623def6
AmmuraHeidar <56014394+AmmuraHeidar@users.noreply.github.com>
2021-03-11 22:42:21 -0800

    Update index.php

--- index.php
+++ index.php
@@ -40,6 +40,21 @@
             <p>We offer Events for Every Body.</p><br><br>
             
             <?php include 'gallery.php' ?><br>
+            
+            <!-- weather widget start -->
+            <a class="weatherwidget-io" href="https://forecast7.com/en/45d52n122d68/portland/?unit=us" data-label_1="PORTLAND" data-label_2="WEATHER" data-theme="original">PORTLAND WEATHER</a>
+            <script>
+                ! function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (!d.getElementById(id)) {
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = 'https://weatherwidget.io/js/widget.min.js';
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }
+                }(document, 'script', 'weatherwidget-io-js');
+            </script>
+            <!-- weather widget end -->
         
 
         </main>
