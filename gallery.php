025e687b40fed12eb7f8e2829d0e6f00830be2da
AmmuraHeidar <56014394+AmmuraHeidar@users.noreply.github.com>
2021-03-16 19:15:54 -0700

    Add files via upload

--- gallery.php
+++ gallery.php
@@ -22,38 +22,48 @@
 
 <body>
 
-        <main>
+    <main>
 
-            <img class="nature" src="images/ace1.jpg" alt="contestants running through shallow water">
-            <img class="nature" src="images/ace2.jpg" alt="boy with walking sticks walking the 5k">
-            <img class="nature" src="images/ace3.jpg" alt="cyclist winning the bike ride">
-            <img class="nature" src="images/ace4.jpg" alt="divers running into the ocean">
-            <img class="nature" src="images/ace5.jpg" alt="cyclists under the sun at noon">
-            <img class="nature" src="images/ace8.jpg" alt="female runner during the sprint">
-            <img class="nature" src="images/ace9.jpg" alt="contestant winning in their wheelchair">
-            <img class="nature" src="images/ace10.jpg" alt="runner reaching the finishline of our try-a-tri">
-            <img class="nature" src="images/ace12.jpg" alt="cyclists frontshot during the olympic bike">
-            <img class="nature" src="images/ace13.jpg" alt="sprinters frontshot during the olympic run">
-            <img class="nature" src="images/ace18.jpg" alt="runners going up under the bridge during the long course run">
+        <div class="row">
+            <div class="col1">
 
-            <script>
-                w3.slideshow(".nature", 4000);
-            </script>
+                <img class="nature" src="images/ace1.jpg" alt="contestants running through shallow water">
+                <img class="nature" src="images/ace2.jpg" alt="boy with walking sticks walking the 5k">
+                <img class="nature" src="images/ace3.jpg" alt="cyclist winning the bike ride">
+                <img class="nature" src="images/ace4.jpg" alt="divers running into the ocean">
+                <img class="nature" src="images/ace5.jpg" alt="cyclists under the sun at noon">
+                <img class="nature" src="images/ace8.jpg" alt="female runner during the sprint">
+                <img class="nature" src="images/ace9.jpg" alt="contestant winning in their wheelchair">
+                <img class="nature" src="images/ace10.jpg" alt="runner reaching the finishline of our try-a-tri">
+                <img class="nature" src="images/ace12.jpg" alt="cyclists frontshot during the olympic bike">
+                <img class="nature" src="images/ace13.jpg" alt="sprinters frontshot during the olympic run">
+                <img class="nature" src="images/ace18.jpg" alt="runners going up under the bridge during the long course run">
 
-            <p>You could be featured in one of these photos too. <a href="registration/index.php" target="_self">Register Today!</a></p><br><br>
+                <script>
+                    w3.slideshow(".nature", 4000);
+                </script>
 
-            <h3>Facebook</h3>
-            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D110671210507359%26id%3D110661963841617&width=500&show_text=true&height=186&appId" width="500" height="186" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe><br><br>
+                <p>You could be featured in one of these photos too. <a href="registration/index.php" target="_self">Register Today!</a></p><br><br>
+
+            </div>
+            
+            <div class="col2">
+
+                <h3>Facebook</h3>
+                <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D110671210507359%26id%3D110661963841617&width=500&show_text=true&height=186&appId" width="500" height="186" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe><br><br>
 
 
-            <h3>Twitter</h3>
-            <blockquote class="twitter-tweet">
-                <p lang="en" dir="ltr">Any weather is training weather!</p>&mdash; CAS 222 (@pcccas222) <a href="https://twitter.com/pcccas222/status/1055566270580445185?ref_src=twsrc%5Etfw">October 25, 2018</a>
-            </blockquote>
-            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                <h3>Twitter</h3>
+                <blockquote class="twitter-tweet">
+                    <p lang="en" dir="ltr">Any weather is training weather!</p>&mdash; CAS 222 (@pcccas222) <a href="https://twitter.com/pcccas222/status/1055566270580445185?ref_src=twsrc%5Etfw">October 25, 2018</a>
+                </blockquote>
+                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+            </div>
+        </div>
 
 
-        </main>
+    </main>
 
     <script type="text/javascript" src=""></script>
 
